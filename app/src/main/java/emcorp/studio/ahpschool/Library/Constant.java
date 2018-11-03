f5fca65ddcfe4dfcdaa0a14a59c163a916f15522
Mhunir <misbakhulmunir11@gmail.com>
2018-11-03 21:28:54 +0700

    no message

--- app/src/main/java/emcorp/studio/ahpschool/Library/Constant.java
+++ app/src/main/java/emcorp/studio/ahpschool/Library/Constant.java
@@ -4,8 +4,9 @@ package emcorp.studio.ahpschool.Library;
  * Created by ASUS on 31/05/2016.
  */
 public class Constant {
-    //API
+    //API REQUEST
     public static final String ROOT_URL = "http://project.misbakhulmunir.com/ahp_school/service/service.php";
     public static final String KEY = "aplikasi-ahp";
     public static final String FUNCTION_LISTSCHOOL = "ListSchool";
+
 }
